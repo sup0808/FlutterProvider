895ba661159f9ff099381ac3427d0366c3ce7edf
supriya gupta <supriyag75@gmail.com>
2024-07-24 07:48:49 +0530

    Implement TextFormField for password show and hide feature -- Use ValueNotifier -- Use ValueListenableBuilder

--- lib/screen/value_notify_listener.dart
+++ lib/screen/value_notify_listener.dart
@@ -7,6 +7,7 @@ class ValueNotifyListener extends StatelessWidget {
   Widget build(BuildContext context) {
     print("build");
     ValueNotifier<int> _counter = ValueNotifier(0);
+    ValueNotifier<bool> toggle = ValueNotifier(false);
     return Scaffold(
       appBar: AppBar(
         title: Text("Dark Theme Demo"),
@@ -14,24 +15,43 @@ class ValueNotifyListener extends StatelessWidget {
         titleTextStyle: TextStyle(fontSize: 25, color: Colors.white),
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ValueListenableBuilder(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: toggle,
+              builder: (context, value, child) {
+                return TextFormField(
+                  obscureText: toggle.value,
+                  decoration: InputDecoration(
+                      hintText: 'Enter the Number',
+                      suffixIcon: InkWell(
+                          onTap: () {
+                            toggle.value = !toggle.value;
+                          },
+                          child: Icon(toggle.value
+                              ? Icons.visibility_off_outlined
+                              : Icons.visibility))),
+                );
+              },
+            ),
+            Center(
+                child: ValueListenableBuilder(
               valueListenable: _counter,
-              builder: (context,value,child){
+              builder: (context, value, child) {
                 return Text(
                   "Subscribe ${_counter.value}",
                   style: TextStyle(fontSize: 30),
                 );
               },
-            )
-          )
-        ],
+            ))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           _counter.value++;
         },
         child: Icon(Icons.add),
